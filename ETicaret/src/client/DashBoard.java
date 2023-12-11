cb8a46198b1d2e5b13940c2d04777924691de442
Busrayrl <busrayral45@gmail.com>
2023-12-11 18:19:10 +0300

    fix dashboard

--- ETicaret/src/client/DashBoard.java
+++ ETicaret/src/client/DashBoard.java
@@ -10,39 +10,36 @@ public class DashBoard extends JFrame {
 	//kullanıcı giriş yaptıktan sonraki işlemler burada gerçekleşecek.
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	public DashBoard(String title) {
+    private JPanel contentPane;
+
+    public DashBoard(String title) {
         super(title);
-        
     }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					DashBoard frame = new DashBoard();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    DashBoard frame = new DashBoard();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public DashBoard() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    public DashBoard() {
+        // Ekranın ortasında büyük bir şekilde açılması için
+        setLocationRelativeTo(null);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-		setContentPane(contentPane);
-		
-	}
+        // Ekranı kapattığınızda programın sona ermemesi için
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+    }
 
 }
